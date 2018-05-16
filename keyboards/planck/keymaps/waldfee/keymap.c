4191e8fd5b060e3ea287293599328835feb45b0c
waldfee <waldfee@users.noreply.github.com>
2018-05-16 09:33:33 +0200

    change around media keys

--- keyboards/planck/keymaps/waldfee/keymap.c
+++ keyboards/planck/keymaps/waldfee/keymap.c
@@ -64,20 +64,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,---------------------------------------------------------------------------------------------.
- * |   ~       |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  | Home | Pg Up| Bksp      |
+ * |   ~       |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  | Home | Pg Up| Del       |
  * |-----------+------+------+------+------+-------------+------+------+------+------+-----------|
- * | Del       |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  | End  | Pg Dn|           |
+ * |           |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  | End  | Pg Dn|           |
  * |-----------+------+------+------+------+------|------+------+------+------+------+-----------|
- * | Shift / { |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      | Shift / } |
+ * | Shift / { |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      | Vol+ | Shift / } |
  * |-----------+------+------+------+------+------+------+------+------+------+------+-----------|
- * |           |      |  App |      |      |             |      | Next | Vol- | Vol+ | Play      |
+ * |           |      |  App |      |      |             |      |      | Next | Vol- | Play      |
  * `---------------------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_HOME, KC_PGUP, KC_BSPC   },
-  {KC_DEL,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_END , KC_PGDN, _______   },
-  {LSFT_LCBR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), _______, _______,  RSFT_RCBR },
-  {_______,   _______, KC_APP, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY   }
+  {KC_TILD,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_HOME, KC_PGUP, KC_DEL    },
+  {_______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_END , KC_PGDN, _______   },
+  {LSFT_LCBR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), _______, KC_VOLU, RSFT_RCBR },
+  {_______,   _______, KC_APP,  _______, _______, _______, _______, _______,    _______,    KC_MNXT, KC_VOLD, KC_MPLY   }
 },
 
 /* Raise
@@ -86,16 +86,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------+------+------+------+------+-------------+------+------+------+------+-----------|
  * | Del       |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \        |
  * |-----------+------+------+------+------+------|------+------+------+------+------+-----------|
- * | Shift / [ |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn | Shift / ] |
+ * | Shift / [ |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      | Shift / ] |
  * |-----------+------+------+------+------+------+------+------+------+------+------+-----------|
- * |           |      |      |      |      |             |      | Next | Vol- | Vol+ | Play      |
+ * |           |      |      |      |      |             |      |      |      |      |           |
  * `---------------------------------------------------------------------------------------------'
  */
 [_RAISE] = {
   {KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC   },
   {KC_DEL,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS   },
-  {LSFT_LBRC, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, RSFT_RBRC },
-  {_______,   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY   }
+  {LSFT_LBRC, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, RSFT_RBRC },
+  {_______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______   }
 },
 
 /* Adjust (Lower + Raise)
