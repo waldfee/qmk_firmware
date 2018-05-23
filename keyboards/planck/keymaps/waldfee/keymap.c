18424d4b35455a54ae17dadbe5658c82e63612bc
waldfee <waldfee@users.noreply.github.com>
2018-05-23 07:58:30 +0200

    fix build

--- keyboards/planck/keymaps/waldfee/keymap.c
+++ keyboards/planck/keymaps/waldfee/keymap.c
@@ -147,10 +147,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	  
     if (is_lalt_active) {
       if (record->event.pressed) {
-          layer_on(_NUMPAD)
+          layer_on(_NUMPAD);
       }
       else {
-         layer_off(_NUMPAD)
+         layer_off(_NUMPAD);
       }
      
       return false;
