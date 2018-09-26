e5fa51fa7c5c9e6a456f99f10e2923322f10413e
waldfee <waldfee@users.noreply.github.com>
2018-09-26 09:24:41 +0200

    swap gui + alt, change numpad activation, add brackets to raise

--- keyboards/planck/keymaps/waldfee/keymap.c
+++ keyboards/planck/keymaps/waldfee/keymap.c
@@ -37,16 +37,8 @@ enum planck_layers {
 #define XXXXXXX KC_NO
 
 #define LOWER MO(_LOWER)
+
 #define RAISE MO(_RAISE)
-
-#define LSFT_LPRN LSFT_T(DE_LPRN) // shift when held, ( otherwise
-#define RSFT_RPRN RSFT_T(DE_RPRN) // shift when held, ) otherwise
-
-#define LSFT_LCBR LSFT_T(DE_LCBR) // shift when held, { otherwise
-#define RSFT_RCBR RSFT_T(DE_RCBR) // shift when held, } otherwise
-
-#define LSFT_LBRC LSFT_T(DE_LBRC) // shift when held, [ otherwise
-#define RSFT_RBRC RSFT_T(DE_RBRC) // shift when held, ] otherwise
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -56,16 +48,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------+------+------+------+------+-------------+------+------+------+------+-----------|
    * | Tab       |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   #  |   +       |
    * |-----------+------+------+------+------+------+------+------+------+------+------+-----------|
-   * | Shift / ( |   Y  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  | Shift / ) |
+   * | Shift     |   Y  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  | Shift     |
    * |-----------+------+------+------+------+------+------+------+------+------+------+-----------|
-   * | Ctrl      | Alt  |  GUI |   <  |Lower |  Spc |  Ent |Raise | AltGr| Left | Down | Right     |
+   * | Ctrl      |  GUI |  Alt |   <  |Lower |  Spc |  Ent |Raise | AltGr| Left | Down | Right     |
    * `---------------------------------------------------------------------------------------------'
    */
   [_QWERTZ] = LAYOUT_planck_grid(
     KC_GESC,   DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    KC_BSPC   ,
     KC_TAB,    DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_HASH, DE_PLUS   ,
-    LSFT_LPRN, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT_RPRN ,
-    KC_LCTL,   KC_LALT, KC_LGUI, DE_LESS, LOWER,   KC_SPC,  KC_ENT,  RAISE,   DE_ALGR, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LSFT,   DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT   ,
+    KC_LCTL,   KC_LGUI, KC_LALT, DE_LESS, LOWER,   KC_SPC,  KC_ENT,  RAISE,   DE_ALGR, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   /* Lower
@@ -74,16 +66,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------+------+------+------+------+-------------+------+------+------+------+-----------|
    * |    _      |  "   |   *  |  /   |   ?  |      |      |      |      | End  | Pg Dn|   -       |
    * |-----------+------+------+------+------+------+------+------+------+------+------+-----------|
-   * | Shift / { |      |      |      |      |   \  |      |      |      |      | Vol+ | Shift / } |
+   * | Shift     |      |      |      |      |   \  |      |      |      |      | Vol+ | Shift / } |
    * |-----------+------+------+------+------+------+------+------+------+------+------+-----------|
-   * |           |      |      |      |      |      |      |      |      | Next | Vol- | Play      |
+   * |           |  ^   |      |      |      |      |      |      |      | Next | Vol- | Play      |
    * `---------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT_planck_grid(
     DE_TILD,   _______, _______, _______, DE_EXLM, DE_ACUT, DE_PERC, DE_AMPR, DE_EQL , KC_HOME, KC_PGUP, KC_DEL    ,
     DE_UNDS,   DE_DQOT, DE_ASTR, DE_SLSH, DE_QST , _______, _______, _______, _______, KC_END , KC_PGDN, DE_MINS   ,
-    LSFT_LCBR, _______, _______, _______, _______, DE_BSLS, _______, _______, _______, _______, KC_VOLU, RSFT_RCBR ,
-    _______,   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_MPLY
+    KC_LSFT,   _______, _______, _______, _______, DE_BSLS, _______, _______, _______, _______, KC_VOLU, KC_RSFT ,
+    _______,   DE_CIRC, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_MPLY
   ),
 
   /* Raise
@@ -92,17 +84,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------+------+------+------+------+-------------+------+------+------+------+-----------|
    * |      1    |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |  10  |      |           |
    * |-----------+------+------+------+------+------+------+------+------+------+------+-----------|
-   * | Shift / [ |      |      |      |      |      |      |      |      |      |      | Shift / ] |
+   * | Shift     |   (  |   )  |   {  |   }  |   [  |   ]  |      |      |      |      | Shift     |
    * |-----------+------+------+------+------+------+------+------+------+------+------+-----------|
-   * |           |      | App  |      |      |      |      |      |      |      |      |           |
+   * |           |  App |      |      |      |      |      |      |      |      |      |           |
    * `---------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT_planck_grid(
     KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12    ,
     KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______   ,
-    LSFT_LBRC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RSFT_RBRC ,
-    _______,   _______, KC_APP,  _______, _______, _______, _______, _______, _______, _______, _______, _______
-  ),
+    KC_LSFT,   DE_LPRN, DE_RPRN, DE_LCBR, DE_RCBR, DE_LBRC, DE_RBRC, _______, _______, _______, _______, KC_RSFT ,
+    _______,   KC_APP,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ), 
 
   /* Umlaut (Lower + Raise)
    * ,-----------------------------------------------------------------------------------------.
@@ -122,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT
   ),
 
-  /* Numpad (Lalt + Esc)
+  /* Numpad (lctrl + Gui)
    * ,-----------------------------------------------------------------------------------.
    * |      |      |      |      |      |   7  |   8  |   9  |   =  |      |      | Ins  |
    * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -169,12 +161,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   
   // activate _NUMPAD
   if (layer == _QWERTZ || layer == _NUMPAD) {
-    bool is_lalt_active = (keyboard_report->mods & MOD_BIT(KC_LALT));
-    if (keycode == KC_ESC && is_lalt_active && layer == _QWERTZ && record->event.pressed) {
+    bool is_lctrl_active = (keyboard_report->mods & MOD_BIT(KC_LCTL));
+    if (keycode == KC_LGUI && is_lctrl_active && layer == _QWERTZ && record->event.pressed) {
       layer_on(_NUMPAD);
       return false;
     }
-    if ((keycode == KC_ESC || keycode == KC_LALT) && layer == _NUMPAD && !record->event.pressed) {
+    if ((keycode == KC_LGUI || keycode == KC_LCTL) && layer == _NUMPAD && !record->event.pressed) {
       layer_off(_NUMPAD);
       return false;
     }
